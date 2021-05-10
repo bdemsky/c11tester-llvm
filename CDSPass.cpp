7a52dd4cf76177b42117bb6eda05815e6b819009
weiyu <weiyuluo1232@gmail.com>
2021-05-10 14:06:51 -0700

    Compiler under llvm 12 release

--- CDSPass.cpp
+++ CDSPass.cpp
@@ -482,7 +482,7 @@ void CDSPass::chooseInstructionsToInstrument(
 		Value *Addr = isa<StoreInst>(*I)
 			? cast<StoreInst>(I)->getPointerOperand()
 			: cast<LoadInst>(I)->getPointerOperand();
-		if (isa<AllocaInst>(GetUnderlyingObject(Addr, DL)) &&
+		if (isa<AllocaInst>(getUnderlyingObject(Addr)) &&
 				!PointerMayBeCaptured(Addr, true, true)) {
 			// The variable is addressable but not captured, so it cannot be
 			// referenced from a different thread and participate in a data race
